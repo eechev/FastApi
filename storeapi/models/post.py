e790dda9efeb8e76f2e127018ac6bfe49bb69621
Elgardo Echevarria <elgardoe@gmail.com>
2024-12-05 15:23:00 -0500

    a fast api app with various end points

---
+++ storeapi/models/post.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+"""_summary_
+
+Models
+
+"""
+
+
+class UserPostIn(BaseModel):
+    body: str
+
+
+class UserPost(UserPostIn):
+    id: int
+
+
+class CommentIn(BaseModel):
+    body: str
+    post_id: int
+
+
+class Comment(CommentIn):
+    id: int
+
+
+class UserPostWithComments(BaseModel):
+    post: UserPost
+    comments: list[Comment]

159d99b996f737e7c9cbef681eb5114881300afa
Elgardo Echevarria <elgardoe@gmail.com>
2024-12-05 23:06:51 -0500

    set up .env and config file

---
+++ storeapi/config.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    ENV_STATE: Optional[str] = None
+
+
+class GlobalConfig(BaseConfig):
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
+
+
+class DevConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="DEV_")
+
+
+class ProdConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="PROD_")
+
+
+class TestConfig(GlobalConfig):
+    DATABASE_URL: Optional[str] = "sqlite:///test.db"
+    DB_FORCE_ROLL_BACK: bool = True
+    model_config = SettingsConfigDict(env_prefix="TEST_")

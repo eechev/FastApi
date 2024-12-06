68b8412ff8b2407a7a0207c194ecd30a9f12b1df
Elgardo Echevarria <elgardoe@gmail.com>
2024-12-05 23:38:57 -0500

    initial database setup

---
+++ storeapi/database.py
@@ -0,0 +1,35 @@
+import databases
+import sqlalchemy
+from config import config
+
+metadata = sqlalchemy.MetaData()
+
+post_table = sqlalchemy.Table(
+    "posts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+)
+
+column_table = sqlalchemy.Table(
+    "comments",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+    sqlalchemy.Column(
+        "post_id", sqlalchemy.Integer, sqlalchemy.ForeignKey("posts.id"), nullable=False
+    ),
+)
+
+
+if config.DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set")
+
+engine = sqlalchemy.create_engine(
+    config.DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+metadata.create_all(engine)
+database = databases.Database(
+    config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
+)

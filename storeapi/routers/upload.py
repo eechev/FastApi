47e763108fd99009321ec997f0fec34a10aabcaf
Elgardo Echevarria <elgardoe@gmail.com>
2024-12-10 14:44:04 -0500

    added test to upload and authentication

--- storeapi/routers/upload.py
+++ storeapi/routers/upload.py
@@ -1,10 +1,13 @@
 import logging
 import tempfile
+from typing import Annotated
 
 import aiofiles
-from fastapi import APIRouter, HTTPException, UploadFile, status
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, status
 
 from storeapi.libs.b2 import b2_upload_file
+from storeapi.models.user import User
+from storeapi.security import get_current_user
 
 logger = logging.getLogger(__name__)
 
@@ -14,11 +17,16 @@ CHUNK_SIZE = 1024 * 1024
 
 
 @router.post("/upload", status_code=status.HTTP_201_CREATED)
-async def upload(file: UploadFile):
+async def upload(
+    file: UploadFile, current_user: Annotated[User, Depends(get_current_user)]
+):
     try:
         with tempfile.NamedTemporaryFile() as temp_file:
             filename = temp_file.name
-            logger.info(f"Saving uploaded file temporarily to {filename}")
+            logger.info(
+                f"User is saving uploaded file temporarily to {filename}",
+                extra={"email": current_user.email},
+            )
             async with aiofiles.open(filename, "wb") as f:
                 while chunk := await file.read(CHUNK_SIZE):
                     await f.write(chunk)
--- storeapi/tests/routers/test_upload.py
+++ storeapi/tests/routers/test_upload.py
@@ -1,5 +1,7 @@
 import contextlib
+import os
 import pathlib
+import tempfile
 
 import pytest
 from fastapi import status
@@ -53,3 +55,24 @@ async def test_upload_image(
     response = await call_upload_endpoint(async_client, logged_in_token, sample_image)
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["file_url"] == "https://fake.url"
+
+
+@pytest.mark.anyio
+async def test_upload_image_no_token(
+    async_client: AsyncClient,
+    sample_image: pathlib.Path,
+):
+    response = await call_upload_endpoint(async_client, "bad_token", sample_image)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.anyio
+async def test_temp_file_removed_after_upload(
+    async_client: AsyncClient, logged_in_token: str, sample_image: pathlib.Path, mocker
+):
+    named_temp_file_spy = mocker.spy(tempfile, "NamedTemporaryFile")
+
+    response = await call_upload_endpoint(async_client, logged_in_token, sample_image)
+    assert response.status_code == status.HTTP_201_CREATED
+    created_temp_file = named_temp_file_spy.spy_return
+    assert not os.path.exists(created_temp_file.name)

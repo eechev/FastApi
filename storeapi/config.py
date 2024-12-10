74d68bee336d70aa1682185d885ecd3717325bac
Elgardo Echevarria <elgardoe@gmail.com>
2024-12-10 16:47:09 -0500

    updates to requirements and config

--- storeapi/config.py
+++ storeapi/config.py
@@ -22,17 +22,17 @@ class GlobalConfig(BaseConfig):
 
 
 class DevConfig(GlobalConfig):
-    model_config = SettingsConfigDict(env_prefix="DEV_")
+    model_config = SettingsConfigDict(env_prefix="DEV_", extra="ignore")
 
 
 class ProdConfig(GlobalConfig):
-    model_config = SettingsConfigDict(env_prefix="PROD_")
+    model_config = SettingsConfigDict(env_prefix="PROD_", extra="ignore")
 
 
 class TestConfig(GlobalConfig):
     DATABASE_URL: Optional[str] = "sqlite:///test.db"
     DB_FORCE_ROLL_BACK: bool = True
-    model_config = SettingsConfigDict(env_prefix="TEST_")
+    model_config = SettingsConfigDict(env_prefix="TEST_", extra="ignore")
 
 
 @lru_cache()
